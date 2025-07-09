9b63ba1293095c9e7be44eaa9050e075fa23a47d
Cornbread2100 <cornbread2100@cornbread2100.com>
2025-07-09 16:24:30 -0400

    postgres, token caching

--- index.js
+++ index.js
@@ -1,110 +1,130 @@
 const fs = require('fs');
 const mineflayer = require('mineflayer');
+const auth = require('prismarine-auth');
 const ping = require('./ping.js');
 const config = require('./config.json');
-const mongoClient = new (require('mongodb').MongoClient)(config.mongoURI);
-let scannedServers;
+const pg = require('pg');
+const { ref } = require('process');
+client = new pg.Client({
+    host: config.sql.host,
+    port: config.sql.port,
+    user: config.sql.user,
+    password: config.sql.password,
+    database: config.sql.database,
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+});
 
-function join(username, password, ip, port, version) {
-  return new Promise((resolve, reject) => {
-    let endTimeout = setTimeout(() => resolve(null), 6000);
+function join(account, ip, port) {
+    return new Promise(async (resolve, reject) => {
+        await refreshToken(account);
+        let endTimeout = setTimeout(() => resolve(null), 6000);
 
-    const bot = mineflayer.createBot({
-      host: ip,
-      port,
-      version,
-      auth: 'microsoft',
-      username: username,
-      password: password
-    })
+        const bot = mineflayer.createBot({
+            host: ip,
+            port,
+            auth: 'microsoft',
+            username: account.username,
+            tokens: account.tokens
+        })
 
-    bot.on('login', async () => {
-      bot.chat('WARNING: If you don\'t want your server to be joined (and likely destroyed) by random people, the only way to protect your server is by enabling a whitelist. Just banning this bot will NOT protect your server.');
-      bot.chat('If this is intended to be a public server, simply ban this bot and my messages will stop.');
-      clearTimeout(endTimeout);
-      endTimeout = setTimeout(() => {
-        bot.end();
-        resolve(false);
-      }, 3000);
+        bot.on('login', async () => {
+            resolve(false);
+            bot.chat('WARNING: If you don\'t want your server to be joined (and likely destroyed) by random people, the only way to protect your server is by enabling a whitelist. Banning this bot will NOT protect your server.');
+            bot.chat('If this is intended to be a public server, simply ban this bot and my messages will stop. DM @cornbread2100 on Discord for more info.');
+            clearTimeout(endTimeout);
+            endTimeout = setTimeout(() => {
+                bot.end();
+                resolve(false);
+            }, 3000);
+        });
+        bot.on('chat', (username, message) => { 
+            if (bot.username != username) return;
+            clearTimeout(endTimeout);
+            bot.end();
+        })
+
+        // Log errors and kick reasons:
+        bot.on('kicked', (reason) => {
+            if (typeof reason == 'object') reason = JSON.stringify(reason);
+            // console.log(`Kicked from ${ip}:${port}`, reason);
+            if (reason.includes('You are not whitelisted on this server') || reason.includes('multiplayer.disconnect.not_whitelisted')) resolve(true);
+            else resolve(null);
+        });
+        bot.on('error', (err) => {
+            // console.log(`Error on ${ip}:${port} ${version}`, err);
+            if (err.message.includes('RateLimiter disallowed request') || err.message.includes('Failed to obtain profile data')) resolve('retry');
+            else resolve(null);
+        });
     });
-    bot.on('chat', (username, message) => { 
-      if (bot.username != username) return;
-      clearTimeout(endTimeout);
-      bot.end();
-      resolve(false);
-    })
-
-    // Log errors and kick reasons:
-    bot.on('kicked', (reason) => {
-      if (typeof reason == 'object') reason = JSON.stringify(reason);
-      // console.log(`Kicked from ${ip}:${port}`, reason);
-      if (reason.includes('You are not whitelisted on this server') || reason.includes('multiplayer.disconnect.not_whitelisted')) resolve(true);
-      else resolve(null);
-    });
-    bot.on('error', (err) => {
-      // console.log(`Error on ${ip}:${port} ${version}`, err);
-      if (err.message.includes('RateLimiter disallowed request') || err.message.includes('Failed to obtain profile data')) resolve('retry');
-      else resolve(null);
-    });
-  });
 }
 
-lastResult = new Date().getTime();
+async function refreshToken(account) {
+    const flow = new auth.Authflow(account.username, './.auth-cache', {
+        flow: 'live',
+        password: account.password,
+        authTitle: auth.Titles.MinecraftJava,
+        deviceType: 'Win32'
+    });
+    account.tokens = await flow.getMinecraftJavaToken({ fetchProfile: true });
+    account.expiration = JSON.parse(Buffer.from(account.tokens.token.split('.')[1], 'base64').toString()).exp;
+}
+
 async function scan() {
-  await mongoClient.connect();
-  scannedServers = mongoClient.db(config.dbName).collection(config.collectionName);
-  let start = new Date().getTime();
-  const ips = config.customIps ? fs.readFileSync(config.ipsPath) : Buffer.from(await (await fetch('https://github.com/kgurchiek/Minecraft-Server-Scanner/raw/main/ips')).arrayBuffer());
-  const startIndex = Math.floor(Math.random() * ips.length / 6) * 6;
-  console.log(`Scanning ${ips.length / 6} servers`);
+    await client.connect();
+    console.log('Connected to database');
+    let ips = config.customIps ? fs.readFileSync(config.ipsPath) : Buffer.from(await (await fetch('https://github.com/kgurchiek/Minecraft-Server-Scanner/raw/main/ips')).arrayBuffer());
+    let startIndex = Math.floor(Math.random() * ips.length / 6) * 6;
+    ips = Buffer.concat([ips.slice(startIndex), ips.slice(0, startIndex)]);
+    console.log(`Scanning ${(ips.length / 6).toLocaleString()} servers`);
 
-  const versions = await (await fetch('https://raw.githubusercontent.com/PrismarineJS/minecraft-data/master/data/pc/common/protocolVersions.json')).json();
-
-  async function check(index) {
-    const account = config.accounts[(index / 6) % config.accounts.length];
-    const ip = `${ips[index]}.${ips[index + 1]}.${ips[index + 2]}.${ips[index + 3]}`;
-    const port = ips[index + 4] * 256 + ips[index + 5];
-    const slp = await ping(ip, port, 0);
-    if (typeof slp == 'string' || slp?.version?.protocol == null) return;
-    const version = versions.find(a => a.version == slp.version.protocol);
-    if (version == null) return;
-    let result;
-    try {
-      result = await join(account.username, account.password, ip, port, version.minecraftVersion);
-    } catch (err) {
-      // console.log(`Bot error on ${ip}:${port}`, err);
-      result = null;
+    async function check(index) {
+        const account = config.accounts[(index / 6) % config.accounts.length];
+        const ip = `${ips[index]}.${ips[index + 1]}.${ips[index + 2]}.${ips[index + 3]}`;
+        const port = ips[index + 4] * 256 + ips[index + 5];
+        const slp = await ping(ip, port, 0);
+        if (typeof slp == 'string' || slp?.version?.protocol == null) return;
+        let result;
+        let oldLog = console.log;
+        let oldWarn = console.warn;
+        let oldError = console.error;
+        if (config.suppressLogs) {
+            console.log = () => {};
+            console.warn = () => {};
+            console.error = () => {};
+        }
+        try {
+            result = await join(account, ip, port);
+        } catch (err) {
+            // console.log(`Bot error on ${ip}:${port}`, err);
+            result = null;
+        }
+        while (result == 'retry') {
+            await new Promise(res => setTimeout(res, 2000));
+            try {
+                result = await join(account, ip, port);
+            } catch (err) {
+                // console.log(`Bot error on ${ip}:${port} ${slp.version.protocol}`, err);
+                result = null;
+            }
+        }
+        if (config.suppressLogs) {
+            console.log = oldLog;
+            console.warn = oldWarn;
+            console.error = oldError;
+        }
+        if (result == null) return;
+        console.log(`${ip}:${port} ${result}`);
+        await client.query('UPDATE SERVERS SET whitelisted = $1 WHERE ip = $2 AND port = $3', [result, ip.split('.').reverse().map((a, i) => parseInt(a) * 256**i).reduce((a, b) => a + b, 0) - 2147483648, port - 32768]);
     }
-    while (result == 'retry') {
-      try {
-        result = await join(account.username, account.password, ip, port, version.minecraftVersion);
-      } catch (err) {
-        // console.log(`Error on ${ip}:${port} ${slp.version.protocol}`, err);
-        result = 'retry';
-      }
-      await new Promise(res => setTimeout(res, 1000));
-    }
-    if (result != null) console.log(`${ip}:${port} ${version.minecraftVersion} ${result} ${(new Date().getTime() - lastResult) / 1000}s`);
-    lastResult = new Date().getTime();
-    const document  = {
-      ip,
-      port,
-      version: slp.version,
-      description: slp.description,
-      enforcesSecureChat: slp.enforcesSecureChat,
-      hasFavicon: slp.favicon != null,
-      hasForgeData: slp.forgeData != null,
-      lastSeen: Math.floor((new Date()).getTime() / 1000)
-    }
-    if (result != null) document.whitelist = result;
-    scannedServers.updateOne({ ip, port }, { $set: document }, { upsert: true });
-    // await new Promise(res => setTimeout(res, 1000));
-  }
 
-  for (let i = startIndex; i < ips.length; i += 6) await check(i);
-  for (let i = 0; i < startIndex; i += 6) await check(i);
-
-  if (config.repeat) setTimeout(scan, 0);
+    for (let i = startIndex; i < ips.length; i += 6) await check(i);
+    for (let i = 0; i < startIndex; i += 6) await check(i);
 }
 
-scan();
+(async () => {
+    for (const account of config.accounts) await refreshToken(account);
+    scan();
+})()
--- send.js
+++ send.js
@@ -2,41 +2,39 @@ const net = require('net');
 const varint = require('varint');
 
 module.exports = (ip, port, packet, timeout = 5000) => {
-  return new Promise(async (resolve, reject) => {
-    var data = Buffer.alloc(0);
-    var length;
-    var hasResponded = false;
+    return new Promise(async (resolve, reject) => {
+        var data = Buffer.alloc(0);
+        var length;
 
-    setTimeout(() => {
-      if (!hasResponded) {
-        client.destroy();
-        resolve('timeout');
-      }
-    }, timeout)
+        let timeoutHandler = setTimeout(() => {
+            client.destroy();
+            resolve('timeout');
+        }, timeout);
 
-    const client = new net.Socket();
-    client.connect(port, ip, () => client.write(packet));
-    client.on('error', err => {
-      client.destroy();
-      resolve(err.toString());
-    });
-    client.on('data', newData => {
-      try {
-        if (data.length == 0) {
-          length = varint.decode(newData);
-          newData = newData.subarray(varint.decode.bytes);
-        }
-        data = Buffer.concat([data, newData]);
-        if (data.length >= length) {
-          data = data.subarray(0, length);
-          hasResponded = true;
-          client.destroy();
-          resolve(data);
-        }
-      } catch (err) {
-        client.destroy();
-        resolve(err.toString());
-      }
+        const client = new net.Socket();
+        client.connect(port, ip, () => client.write(packet));
+        client.on('error', err => {
+            clearTimeout(timeoutHandler);
+            client.destroy();
+            resolve(err.toString());
+        });
+        client.on('data', newData => {
+            try {
+                if (data.length == 0) {
+                    length = varint.decode(newData);
+                    newData = newData.subarray(varint.decode.bytes);
+                }
+                data = Buffer.concat([data, newData]);
+                if (data.length >= length) {
+                    data = data.subarray(0, length);
+                    clearTimeout(timeoutHandler);
+                    client.destroy();
+                    resolve(data);
+                }
+            } catch (err) {
+                client.destroy();
+                resolve(err.toString());
+            }
+        })
     })
-  })
 }
